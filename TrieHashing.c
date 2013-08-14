e3493975d7b2826be2d6b8823b4376cdf4aeea70
nnsolutions <nnsolutions@nnsolutions-laptop.(none)>
2013-08-14 17:03:37 -0300

    primeiro commit

---
+++ TrieHashing.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+	int op;
+	
+	scanf("%c",&op);
+	while (op != 'e') {
+		switch (op) {
+			case 'i':
+				break;
+			case 't':
+				break;
+			case 'a':
+				break;
+			case 'r':
+				break;
+			case 'f':
+				break;
+			case 'o':
+				break;
+			case 'p':
+				break;
+			case 'd':
+				break;
+			case 'e':
+				return 0;
+				break;
+			default:
+				break;
+		}
+		scanf("%c",&op);
+	}
+	
+	return 0;
+}
+
